9126d64a366784814f7d4bcc8c3dd6fc71cbe520
fdzul <felipe.dzul.m@gmail.com>
2025-01-28 07:06:04 -0600

    This commit update the file

---
+++ cadenas_transmision.R
@@ -0,0 +1,11 @@
+
+load("/Users/felipedzul/Library/CloudStorage/OneDrive-Personal/proyects/geocoding_mex/2024/8.RData/denmex_2024.RData")
+library(magrittr)
+denhotspots::transmission_chains_map(geocoded_dataset = z,
+                                     cve_edo = "14",
+                                     locality = c("Guadalajara", "Tlaquepaque",
+                                                  "Zapopan", "Tonalá"),
+                                     dengue_cases = "Confirmado")
+
+
+
